e72642db52d56b39ea5602e61902e94d94ed0835
Kairat <ischenbaew@gmai.com>
2024-07-18 01:17:49 +0200

    добавлены модели

--- todo_tracker/tracker/models.py
+++ todo_tracker/tracker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Type(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.summary
