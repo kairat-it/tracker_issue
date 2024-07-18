78b6a35756cefe1cb28a138f4a795441fd5b4536
Kairat <ischenbaew@gmai.com>
2024-07-18 02:10:18 +0200

    добавлены связи "Многие-ко-многим" для типа задачи В tracker/models.py изменил модель Issue

--- todo_tracker/tracker/models.py
+++ todo_tracker/tracker/models.py
@@ -18,9 +18,10 @@ class Issue(models.Model):
     summary = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    types = models.ManyToManyField(Type)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.summary
+
