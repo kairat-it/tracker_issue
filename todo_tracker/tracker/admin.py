827b7598895b16917a7f0a7550760e0da30d2655
Kairat <ischenbaew@gmai.com>
2024-08-01 10:27:05 +0200

    тестовый проект

--- todo_tracker/tracker/admin.py
+++ todo_tracker/tracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Status, Type, Issue
+from .models import Status, Type, Issue, Project
 
 admin.site.register(Status)
 admin.site.register(Type)
 admin.site.register(Issue)
+admin.site.register(Project)
