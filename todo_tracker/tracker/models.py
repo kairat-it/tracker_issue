bf9ffa09df4afd8214e0f1e2f29c94bd2839a4a4
Kairat <ischenbaew@gmai.com>
2024-08-01 10:12:55 +0200

    связал задачи с проектом

--- todo_tracker/tracker/models.py
+++ todo_tracker/tracker/models.py
@@ -15,8 +15,19 @@ class Type(models.Model):
         return self.name
 
 
+class Project(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Issue(models.Model):
     objects = None
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=1)
     summary = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
@@ -28,11 +39,4 @@ class Issue(models.Model):
         return self.summary
 
 
-class Project(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField(blank=True, null=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
 
-    def __str__(self):
-        return self.name
