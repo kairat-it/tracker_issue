ea8da26eeca2c37c8fad0e0a4cd868811794058c
Kairat <ischenbaew@gmai.com>
2024-08-01 11:15:08 +0200

    добавлены пути для проектов

--- todo_tracker/tracker/urls.py
+++ todo_tracker/tracker/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import IssueListView, IssueDetailView, IssueCreateView, IssueUpdateView, IssueDeleteView
+from .views import IssueListView, IssueDetailView, IssueCreateView, IssueUpdateView, IssueDeleteView, ProjectListView, \
+    ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 urlpatterns = [
-    path('', IssueListView.as_view(), name='issue_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('project/add/', ProjectCreateView.as_view(), name='project_add'),
+    path('project/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
+    path('project/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
     path('issue/<int:pk>/', IssueDetailView.as_view(), name='issue_detail'),
     path('issue/add/', IssueCreateView.as_view(), name='issue_add'),
     path('issue/<int:pk>/edit/', IssueUpdateView.as_view(), name='issue_edit'),
